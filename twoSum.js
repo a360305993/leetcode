c49b1396cdfeb92a4477074809ec40a133f6ac5a
a360305993 <360305993@qq.com>
2016-11-23 12:07:18 +0800

    towsum

---
+++ twoSum.js
@@ -0,0 +1,34 @@
+/*Given an array of integers, return indices of the two numbers such that 
+they add up to a specific target.
+
+You may assume that each input would have exactly one solution.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+
+The return format had been changed to zero-based indices. 
+
+
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+   /*for(var i=0;i<nums.length;i++)
+		for(var j=i+1;j<nums.length;j++){
+			if(nums[i]+nums[j]===target){
+				return [i,j]
+			}
+		}*/
+	var hash=[];
+	for(var i=0;i<nums.length;i++){
+		var temp=target-nums[i];
+		if(hash[temp]!==undefined){return [i,hash[temp]]}
+		hash[nums[i]]=i;
+	}
+};
+
+console.log(twoSum([2,7,11,15],9))

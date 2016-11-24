04d8b4cf76da8575076d8d7a29e75aad9738b674
a360305993 <360305993@qq.com>
2016-11-24 12:23:41 +0800

    use pareint,cheating

--- atoi.js
+++ atoi.js
@@ -6,11 +6,13 @@ var myAtoi = function(str) {
     var MAXVALUE=2147483647;
 	var MINVALUE=-MAXVALUE-1;
     var r=parseInt(str);
+    r=r?r:0;//NAN to 0
     if(r>MAXVALUE){return MAXVALUE;}
     if(r<MINVALUE){return MINVALUE;}
     return r;
 };
 
+console.log(myAtoi(''))
 
 
 
